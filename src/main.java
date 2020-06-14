b82f6564a75c92357703dda1127f2e9069df80ef
dimas.lourenco@gertec.com.br <Dimas2310>
2020-06-13 23:46:29 -0300

    Metodo GetRequest da api

--- src/main.java
+++ src/main.java
@@ -1,67 +1,6 @@
-import java.util.concurrent.TimeUnit;
-
-import org.apache.hc.core5.http.ClassicHttpRequest;
-import org.apache.hc.core5.http.ClassicHttpResponse;
-import org.apache.hc.core5.http.HttpConnection;
-import org.apache.hc.core5.http.HttpHost;
-import org.apache.hc.core5.http.HttpRequest;
-import org.apache.hc.core5.http.HttpResponse;
-import org.apache.hc.core5.http.Method;
-import org.apache.hc.core5.http.impl.Http1StreamListener;
-import org.apache.hc.core5.http.impl.bootstrap.HttpRequester;
-import org.apache.hc.core5.http.impl.bootstrap.RequesterBootstrap;
-import org.apache.hc.core5.http.io.SocketConfig;
-import org.apache.hc.core5.http.io.entity.EntityUtils;
-import org.apache.hc.core5.http.message.BasicClassicHttpRequest;
-import org.apache.hc.core5.http.message.RequestLine;
-import org.apache.hc.core5.http.message.StatusLine;
-import org.apache.hc.core5.http.protocol.HttpCoreContext;
-import org.apache.hc.core5.util.Timeout;
-
 public class main {
 
-	public static void main(String[] args) throws Exception {
-		final HttpRequester httpRequester = RequesterBootstrap.bootstrap()
-                .setStreamListener(new Http1StreamListener() {
+	public static void main(String[] args){
 
-                    @Override
-                    public void onRequestHead(final HttpConnection connection, final HttpRequest request) {
-                        System.out.println(connection.getRemoteAddress() + " " + new RequestLine(request));
-
-                    }
-
-                    @Override
-                    public void onResponseHead(final HttpConnection connection, final HttpResponse response) {
-                        System.out.println(connection.getRemoteAddress() + " " + new StatusLine(response));
-                    }
-
-                    @Override
-                    public void onExchangeComplete(final HttpConnection connection, final boolean keepAlive) {
-                        if (keepAlive) {
-                            System.out.println(connection.getRemoteAddress() + " exchange completed (connection kept alive)");
-                        } else {
-                            System.out.println(connection.getRemoteAddress() + " exchange completed (connection closed)");
-                        }
-                    }
-
-                })
-                .setSocketConfig(SocketConfig.custom()
-                        .setSoTimeout(5, TimeUnit.SECONDS)
-                        .build())
-                .create();
-
-        final HttpCoreContext coreContext = HttpCoreContext.create();
-        final HttpHost target = new HttpHost("httpbin.org");
-        final String[] requestUris = new String[] {"/", "/ip", "/user-agent", "/headers"};
-
-        for (int i = 0; i < requestUris.length; i++) {
-            final String requestUri = requestUris[i];
-            final ClassicHttpRequest request = new BasicClassicHttpRequest(Method.GET, target, requestUri);
-            try (ClassicHttpResponse response = httpRequester.execute(target, request, Timeout.ofSeconds(5), coreContext)) {
-                System.out.println(requestUri + "->" + response.getCode());
-                System.out.println(EntityUtils.toString(response.getEntity()));
-                System.out.println("==============");
-            }
-        }
-	}
+    }
 }
--- src/requisicaoHttp.java
+++ src/requisicaoHttp.java
@@ -1,53 +1,21 @@
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.hc.client5.http.classic.methods.HttpGet;
-import org.apache.hc.client5.http.classic.methods.HttpPost;
-import org.apache.hc.client5.http.entity.UrlEncodedFormEntity;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.core5.http.HttpEntity;
-import org.apache.hc.core5.http.NameValuePair;
-import org.apache.hc.core5.http.io.entity.EntityUtils;
-import org.apache.hc.core5.http.message.BasicNameValuePair;
+import java.net.*;
+import java.io.*;
 
 public class requisicaoHttp {
-	{
-	try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
-	    HttpGet httpGet = new HttpGet("http://httpbin.org/get");
-	    // The underlying HTTP connection is still held by the response object
-	    // to allow the response content to be streamed directly from the network socket.
-	    // In order to ensure correct deallocation of system resources
-	    // the user MUST call CloseableHttpResponse#close() from a finally clause.
-	    // Please note that if response content is not fully consumed the underlying
-	    // connection cannot be safely re-used and will be shut down and discarded
-	    // by the connection manager.
-	    try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
-	        System.out.println(response1.getCode() + " " + response1.getReasonPhrase());
-	        HttpEntity entity1 = response1.getEntity();
-	        // do something useful with the response body
-	        // and ensure it is fully consumed
-	        EntityUtils.consume(entity1);
-	    }
+	
+	public static String GetRequest(String token)throws Exception {
+		URL url = new URL("https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=" + token);
+	    URLConnection yc = url.openConnection();
+	    BufferedReader in = new BufferedReader(
+	                            new InputStreamReader(
+	                            yc.getInputStream()));
+	    String inputLine;
 
-	    HttpPost httpPost = new HttpPost("http://httpbin.org/post");
-	    List<NameValuePair> nvps = new ArrayList<>();
-	    nvps.add(new BasicNameValuePair("username", "vip"));
-	    nvps.add(new BasicNameValuePair("password", "secret"));
-	    httpPost.setEntity(new UrlEncodedFormEntity(nvps));
-
-	    try (CloseableHttpResponse response2 = httpclient.execute(httpPost)) {
-	        System.out.println(response2.getCode() + " " + response2.getReasonPhrase());
-	        HttpEntity entity2 = response2.getEntity();
-	        // do something useful with the response body
-	        // and ensure it is fully consumed
-	        EntityUtils.consume(entity2);
-	    }
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+	    while ((inputLine = in.readLine()) != null) 
+	        System.out.println(inputLine);
+	    in.close();
+	    
+	    return inputLine;
 	}
-}
+
 }
